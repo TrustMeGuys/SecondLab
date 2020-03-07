68a31a2db559a319924df0e3d1f1665327b7b038
Egor Bozhko <vanya.sosister@mail.ru>
2020-03-07 12:06:23 +0700

    Create proverka diametra

--- gm.c
+++ gm.c
@@ -1,5 +1,14 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+int proverka(float diametr)
+{
+    if (diametr <= 0)
+        return 1;
+    else
+        return 0;
+}
+
 int top(int x)
 {
     if (x == 0) {
@@ -20,13 +29,24 @@ int main()
     printf("Введите данные круга:\n");
     x1 = top(x1);
     y1 = top(x1);
+
     float diameter;
-    printf("Введите диметр коружности:\n ");
+    printf("Введите диаметр коружности:\n ");
     scanf("%f", &diameter);
-    float area = (pi * diameter * diameter) / 4;
-    float perimetr = diameter / 2;
+    int b = proverka(diameter);
+    float area;
+    float perimetr;
+    if (b == 1) {
+        printf("Вы ввели отрицательный или нулевой диаметр\n");
+        return 1;
+    } else {
+        area = (pi * diameter * diameter) / 4;
+        perimetr = diameter / 2;
+    }
     int a[2] = {x1, y1};
+
     printf("circle(%d %d,%f)\n", a[0], a[1], diameter);
     printf("perimetr = %f\n", perimetr);
     printf("area=%f\n", area);
+    return 0;
 }
